413933e558c83263d507e22cb1952ec7dff29ac9
nkazakova <natalia.kazakova@innoway.ru>
2019-07-01 09:30:37 +0300

    Добавлено расширение для Date: Date.humanizeDiff

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,5 +1,6 @@
 package ru.skillbranch.devintensive.extensions
 
+import java.lang.Math.abs
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -31,6 +32,73 @@ fun Date.add(value: Int, units: TimeUnits): Date {
     this.time = time
 
     return this
+}
+
+fun Date.humanizeDiff(date: Date = Date()): String {
+    val timeDiff = this.time - date.time
+
+    val timeDiffInSeconds = abs(timeDiff / SECOND)
+    val timeDiffInMinutes = abs(timeDiff / MINUTE)
+    val timeDiffInHours = abs(timeDiff / HOUR)
+    val timeDiffInDays = abs(timeDiff / DAY)
+
+    val isFuture = timeDiff > 0
+
+    return when {
+        timeDiffInSeconds in 0..1 -> "только что"
+        timeDiffInSeconds in 0..45 && !isFuture -> "несколько секунд назад"
+        timeDiffInSeconds in 0..45 -> "через несколько секунд"
+        timeDiffInSeconds in 45..75 && !isFuture -> "минуту назад"
+        timeDiffInSeconds in 45..75 -> "через минуту"
+        timeDiffInSeconds > 75 && timeDiffInMinutes < 45 -> getTimeStr(timeDiffInMinutes, TimeUnits.MINUTE, isFuture)
+        timeDiffInMinutes in 45..75 && !isFuture -> "час назад"
+        timeDiffInMinutes in 45..75 -> "через час"
+        timeDiffInMinutes > 75 && timeDiffInHours < 22 -> getTimeStr(timeDiffInHours, TimeUnits.HOUR, isFuture)
+        timeDiffInHours in 22..26 && !isFuture -> "день назад"
+        timeDiffInHours in 22..26 -> "через день"
+        timeDiffInHours > 26 && timeDiffInDays < 360 -> getTimeStr(timeDiffInDays, TimeUnits.DAY, isFuture)
+        timeDiffInDays > 360 && !isFuture -> "более года назад"
+        timeDiffInDays > 360 -> "более чем через год"
+        else -> "неизвестно"
+    }
+}
+
+fun getAddition(num: Long, units: TimeUnits): String {
+    val toInt = (num % 10).toInt()
+
+    fun minuteAddition(): String = when (toInt) {
+        1 -> "минуту"
+        in 2..4 -> "минуты"
+        else -> "минут"
+    }
+
+    fun hourAddition(): String = when (toInt) {
+        1 -> "час"
+        in 2..4 -> "часа"
+        else -> "часов"
+    }
+
+    fun dayAddition(): String = when (toInt) {
+        1 -> "день"
+        in 2..4 -> "дня"
+        else -> "дней"
+    }
+
+    return when (units) {
+        TimeUnits.MINUTE -> minuteAddition()
+        TimeUnits.HOUR -> hourAddition()
+        TimeUnits.DAY -> dayAddition()
+        else -> ""
+    }.toString()
+}
+
+fun getTimeStr(num: Long, units: TimeUnits, isFuture: Boolean): String {
+    val addition = getAddition(num, units)
+
+    return when {
+        isFuture -> "через $num $addition"
+        else -> "$num $addition назад"
+    }
 }
 
 enum class TimeUnits {
