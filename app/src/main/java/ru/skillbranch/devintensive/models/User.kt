b7d0a7952ae4c1d842913f7a195207d6d10e8116
nkazakova <natalia.kazakova@innoway.ru>
2019-07-01 15:13:48 +0300

    Реализован паттерн Builder для класса User

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -29,6 +29,17 @@ data class User(
         lastName = "Doe $id"
     )
 
+    private constructor(builder: Builder) : this(
+        id = builder.id,
+        firstName = builder.firstName,
+        lastName = builder.lastName,
+        avatar = builder.avatar,
+        rating = builder.rating,
+        respect = builder.respect,
+        lastVisit = builder.lastVisit,
+        isOnline = builder.isOnline
+    )
+
     companion object Factory {
         private var lastId: Int = -1
 
@@ -39,5 +50,34 @@ data class User(
 
             return User(id = "$lastId", firstName = firstName, lastName = lastName)
         }
+    }
+
+    class Builder {
+        internal var id: String = UUID.randomUUID().toString()
+            private set
+        internal var firstName: String? = null
+            private set
+        internal var lastName: String? = null
+            private set
+        internal var avatar: String? = null
+            private set
+        internal var rating: Int = 0
+            private set
+        internal var respect: Int = 0
+            private set
+        internal var lastVisit: Date? = Date()
+            private set
+        internal var isOnline: Boolean = false
+            private set
+
+        fun id(id: String) = apply { this.id = id }
+        fun firstName(firstName: String?) = apply { this.firstName = firstName }
+        fun lastName(lastName: String?) = apply { this.lastName = lastName }
+        fun avatar(avatar: String?) = apply { this.avatar = avatar }
+        fun rating(rating: Int) = apply { this.rating = rating }
+        fun respect(respect: Int) = apply { this.respect = respect }
+        fun lastVisit(lastVisit: Date?) = apply { this.lastVisit = lastVisit }
+        fun isOnline(isOnline: Boolean) = apply { this.isOnline = isOnline }
+        fun build() = User(this)
     }
 }
