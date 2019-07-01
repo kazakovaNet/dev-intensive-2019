3c59173e30299f143a52f16593c1739d0cf3970d
nkazakova <natalia.kazakova@innoway.ru>
2019-07-01 12:55:21 +0300

    Добавлен data mapping User->UserView

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -0,0 +1,25 @@
+package ru.skillbranch.devintensive.extensions
+
+import ru.skillbranch.devintensive.models.User
+import ru.skillbranch.devintensive.utils.UserView
+import ru.skillbranch.devintensive.utils.Utils
+
+/**
+ * Created by nkazakova on 2019-06-30.
+ */
+
+fun User.toUserView(): UserView {
+    val nickName = Utils.transliteration("$firstName $lastName")
+    val initials = Utils.initials(firstName, lastName)
+    val status =
+        if (lastVisit == null) "Еще ни разу не был" else if (isOnline) "online" else "Последний раз был ${lastVisit?.humanizeDiff()}"
+
+    return UserView(
+        id = id,
+        fullName = "$firstName $lastName",
+        nickName = nickName,
+        initials = initials,
+        avatar = avatar,
+        status = status
+    )
+}
---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/UserView.kt
@@ -0,0 +1,22 @@
+package ru.skillbranch.devintensive.utils
+
+/**
+ * Created by nkazakova on 2019-06-30.
+ */
+class UserView(
+    val id: String,
+    val fullName: String,
+    val nickName: String,
+    val avatar: String? = null,
+    val status: String = "offline",
+    val initials: String?
+) {
+    fun printMe() = println("""
+        id: $id
+        fullName: $fullName
+        nickName: $nickName
+        avatar: $avatar
+        status: $status
+        initials: $initials
+    """.trimIndent())
+}
