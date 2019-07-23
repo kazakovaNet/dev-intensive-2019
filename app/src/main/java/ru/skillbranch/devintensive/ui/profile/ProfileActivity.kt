06a551057006b8e848847df417b5dc12221c71ee
nkazakova <natalia.kazakova@innoway.ru>
2019-07-23 12:14:35 +0300

    Верстка экрана профиля

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -0,0 +1,32 @@
+package ru.skillbranch.devintensive.ui.profile
+
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.models.Bender
+
+class ProfileActivity : AppCompatActivity() {
+
+    lateinit var benderImage: ImageView
+    lateinit var textTxt: TextView
+    lateinit var messageEt: EditText
+    lateinit var sendBtn: ImageView
+
+    lateinit var benderObj: Bender
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+
+    }
+}
