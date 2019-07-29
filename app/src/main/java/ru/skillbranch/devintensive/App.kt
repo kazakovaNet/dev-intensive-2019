6d7aec6bc5f2bf8d665567841dbc193a1ce98ff0
nkazakova <natalia.kazakova@innoway.ru>
2019-07-29 11:51:33 +0300

    Сохранение состояния application, profile screen

---
+++ app/src/main/java/ru/skillbranch/devintensive/App.kt
@@ -0,0 +1,33 @@
+package ru.skillbranch.devintensive
+
+import android.app.Application
+import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
+import ru.skillbranch.devintensive.repositories.PreferencesRepository
+
+/**
+ * Created by nkazakova on 2019-07-28.
+ */
+class App: Application() {
+
+    companion object {
+        private var instance: App? = null
+
+        fun applicationContext():Context {
+            return instance!!.applicationContext
+        }
+    }
+
+    init {
+        instance = this
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        PreferencesRepository.getAppTheme().also {
+            AppCompatDelegate.setDefaultNightMode(it)
+        }
+    }
+
+}
